d5a2fea091428a9b2ffc2b99f4d548cf18c1c81d
Ignace Dewulf <ignace.dewulf2@gmail.com>
2026-02-19 17:28:10 +0100

    fixed the memory leaks from before

--- Minigin/Component.h
+++ Minigin/Component.h
@@ -9,6 +9,12 @@ namespace dae
 	public:
 		Component(GameObject* const owner);
 
+		virtual ~Component() = default;
+		Component(const Component& other) = delete;
+		Component(Component&& other) = delete;
+		Component& operator=(const Component& other) = delete;
+		Component& operator=(Component&& other) = delete;
+
 		virtual void FixedUpdate(float fixedFrameTime);
 		virtual void Update(float deltaTime);
 		virtual void Render() const;
--- Minigin/GameObject.h
+++ Minigin/GameObject.h
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <unordered_map>
 #include <typeindex>
-#include <stdexcept>
 
 #include "Component.h"
 
